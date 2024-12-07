91c82d182d6778f6e2af52af5aecba007ffdd839
Sam Kerr <sam.p.kerr@gmail.com>
2024-12-07 20:27:59 +0000

    day 3

--- README.md
+++ README.md
@@ -1,20 +1,36 @@
 # Advent of Code 2024 - Typescript
 
+![Static Badge](https://img.shields.io/badge/language-typescript-blue)
+
+![Static Badge](https://img.shields.io/badge/days_completed-3-green)
+
+![Static Badge](https://img.shields.io/badge/christmas_saved-false-red)
+
 ### Getting Started
-1. **Clone the Repository**  
+
+1. **Clone the Repository**
 
 ```bash
 git clone git@github.com:SamKerr/advent-of-code-2024-typescript.git
 ```
 
-2. Install Dependencies 
+2. **Install Dependencies**
+
+Check you have npm/ node installed, you can download [node here](https://nodejs.org/en)
+
+```bash
+node -v
+npm -v
+```
+
+Then run
 
 ```bash
 npm install
 ```
 
-
 ### Running the Program
+
 ```bash
 npm run operation-save-christmas --path=[1-25]
 ```
---
+++ src/day3/solution.ts
@@ -0,0 +1,52 @@
+import * as fs from 'fs';
+
+class Solution {
+    private input: string;
+    private sumPattern = /mul\((\d{1,3}),(\d{1,3})\)/g;
+    private doPattern = /do\(\)/g;
+    private dontPattern = /don't\(\)/g;
+    private allOpsPattern = /mul\((\d{1,3}),(\d{1,3})\)|do\(\)|don't\(\)/g;
+
+    constructor() {
+        this.input = fs.readFileSync('src/day3/input.txt', 'utf8');
+    }
+
+    part1(): number {
+        const instructions = [...this.input.matchAll(this.sumPattern)];
+        if (!instructions) throw 'No instructions found in input';
+        return instructions?.reduce((acc, match) => {
+            const operand1 = match[1];
+            const operand2 = match[2];
+            if (isNaN(parseInt(operand1)) || isNaN(parseInt(operand2)))
+                throw `cannot parse numbers ${operand1} ${operand2}`;
+            return acc + parseInt(operand1) * parseInt(operand2);
+        }, 0);
+    }
+
+    part2(): number {
+        const instructions = [...this.input.matchAll(this.allOpsPattern)];
+        if (!instructions) throw 'No instructions found in input';
+        let allOps = true;
+        return instructions?.reduce((acc, match) => {
+            if (match[0].match(this.sumPattern) && allOps) {
+                const operand1 = match[1];
+                const operand2 = match[2];
+                if (isNaN(parseInt(operand1)) || isNaN(parseInt(operand2)))
+                    throw `cannot parse numbers ${operand1} ${operand2}`;
+                return acc + parseInt(operand1) * parseInt(operand2);
+            } else if (match[0].match(this.doPattern)) {
+                allOps = true;
+                return acc;
+            } else if (match[0].match(this.dontPattern)) {
+                allOps = false;
+                return acc;
+            } else {
+                return acc;
+            }
+        }, 0);
+    }
+}
+
+const solution = new Solution();
+console.log(`Solution 1: ${solution.part1()}`);
+console.log(`Solution 2: ${solution.part2()}`);
