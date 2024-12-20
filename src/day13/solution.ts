82dc5f54d2a2a5b047eb86c36a6c2c6fc1fb6789
Sam Kerr <sam.p.kerr@gmail.com>
2024-12-20 00:20:00 +0000

    add some parsing utils, solve day13

---
+++ src/day13/solution.ts
@@ -0,0 +1,92 @@
+import { requestInput } from '$src/utils/http';
+import { extractNumbers } from '$src/utils/parsing';
+
+type Vec2D = number[];
+type Matrix2D = number[][];
+type Problem = {
+    vecA: Vec2D;
+    vecB: Vec2D;
+    target: Vec2D;
+};
+
+function dot(v1: Vec2D, v2: Vec2D): number {
+    return v1.map((v, i) => v * v2[i]).reduce((acc, x) => acc + x, 0);
+}
+
+function matrixMultply(X: Matrix2D, T: Vec2D): Vec2D {
+    return X.map((row) => dot(row as Vec2D, T));
+}
+
+function isRoughlyInteger(num, tolerance = 1e-3) {
+    return Math.abs(num - Math.round(num)) < tolerance;
+}
+
+function parseInput(input: string, isPart1): Problem[] {
+    const problems: Problem[] = [];
+    let curr = {};
+    const rows = input.split('\n');
+    for (const row of rows) {
+        console.log(row);
+        if (row.length === 0) {
+            continue;
+        } else {
+            const vector = row.split(':')[1].split(',').map(extractNumbers);
+            if (row.startsWith('Prize')) {
+                curr = {
+                    ...curr,
+                    target: isPart1
+                        ? vector
+                        : vector.map((v) => v + 10000000000000),
+                };
+                problems.push(curr as Problem);
+            } else if (row.startsWith('Button A:')) {
+                curr = {
+                    ...curr,
+                    vecA: vector,
+                };
+            } else {
+                curr = {
+                    ...curr,
+                    vecB: vector,
+                };
+            }
+        }
+    }
+    return problems;
+}
+
+function solver(input: string, isPart1: boolean): number {
+    const problems = parseInput(input, isPart1);
+    let tokenCount = 0;
+    for (const { vecA, vecB, target } of problems) {
+        // use linear algebra to solve for [vecA, vecB]^(transposed) x N = T, find N
+        const [a, c] = vecA;
+        const [b, d] = vecB;
+
+        // find det
+        const det = a * d - b * c;
+        if (det === 0) {
+            // no solution is possible for system of equations
+            continue;
+        }
+
+        // find inverse matrix
+        const X_inv = [
+            [d, -b],
+            [-c, a],
+        ].map((arr) => arr.map((v) => v * (1 / det)));
+
+        // use matrix multiplication to find N
+        const N = matrixMultply(X_inv, target);
+        if (!N.every((v) => isRoughlyInteger(v))) {
+            // we only count whole number of vectors
+            continue;
+        }
+        tokenCount += 3 * N[0] + N[1];
+    }
+    return tokenCount;
+}
+
+const rawInput = await requestInput(13);
+console.log(solver(rawInput, true));
+console.log(solver(rawInput, false));
---
+++ src/utils/parsing.ts
@@ -0,0 +1,33 @@
+export function extractNumbers(input: string): number {
+    const regex = /\d+/g; // Matches sequences of digits
+    const matches = input.match(regex); // Find all matches
+
+    if (!matches) {
+        throw new Error(`failed to extract numbers from string ${input}`);
+    }
+
+    return parseInt(matches[0]);
+}
+
+export function parseAsOneStrArray(input: string): string[] {
+    return input
+        .split('\n')
+        .filter((arr) => arr.length > 0)
+        .map((s) => s.split(' '))
+        .flat();
+}
+
+export function parseAsStrGrid(input: string): string[][] {
+    return input
+        .split('\n')
+        .filter((arr) => arr.length > 0)
+        .map((arr) => arr.split(''));
+}
+
+export function parseAsIntArray(input: string): number[] {
+    return parseAsOneStrArray(input).map((s) => parseInt(s));
+}
+
+export function parseAsIntGrid(input: string): number[][] {
+    return parseAsStrGrid(input).map((arr) => arr.map((s) => parseInt(s)));
+}
